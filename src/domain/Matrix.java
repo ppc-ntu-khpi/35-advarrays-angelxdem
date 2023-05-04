cafc513ab19f54b76b909e9c0eb0fb174d2362bb
Дем‘яненко Ангеліна <113301385+angelxdem@users.noreply.github.com>
2023-05-04 11:20:51 +0000

    edit  Matrix

--- src/domain/Matrix.java
+++ src/domain/Matrix.java
@@ -1,54 +1,54 @@
 import java.util.Random;
 
 public class Matrix {
-    private int[][] matrix;
+    private final Array array;
 
     public Matrix(int rows, int cols) {
-        matrix = new int[rows][cols];
+        array = new Array(rows, cols);
     }
 
     public Matrix(int rows, int cols, int maxValue) {
-        matrix = new int[rows][cols];
+        array = new Array(rows, cols);
         Random random = new Random();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                matrix[i][j] = random.nextInt(maxValue);
+                array.setElement(i, j, random.nextInt(maxValue));
             }
         }
     }
 
     public void setElement(int row, int col, int value) {
-        matrix[row][col] = value;
+        array.setElement(row, col, value);
     }
 
     public int getElement(int row, int col) {
-        return matrix[row][col];
+        return array.getElement(row, col);
     }
 
     public int getRows() {
-        return matrix.length;
+        return array.getRows();
     }
 
     public int getColumns() {
-        return matrix[0].length;
+        return array.getColumns();
     }
 
     public void transpose() {
         int rows = getRows();
         int cols = getColumns();
-        int[][] transposedMatrix = new int[cols][rows];
+        Array transposedArray = new Array(cols, rows);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                transposedMatrix[j][i] = matrix[i][j];
+                transposedArray.setElement(j, i, array.getElement(i, j));
             }
         }
-        matrix = transposedMatrix;
+        array.copyFrom(transposedArray);
     }
 
     public void printMatrix() {
         for (int i = 0; i < getRows(); i++) {
             for (int j = 0; j < getColumns(); j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(array.getElement(i, j) + " ");
             }
             System.out.println();
         }
